de0edbfaa30116effb822e3fc3b80d5d16061ee7
MarcoStucchi <marco.stucchi@gmail.com>
2025-02-27 03:01:27 +0100

    fix linker and startup files

--- startup/startup.c
+++ startup/startup.c
@@ -18,3 +18,30 @@ uint32_t *vector_table[] = {
     (uint32_t *)HardFault_Handler, /* HardFault */
     /* Add more handlers as needed */
 };
+
+extern int main();
+
+/* Reset handler */
+void Reset_Handler(void) {
+
+    /* Initialize data and bss */
+    extern uint32_t _data_loadaddr, __data_start__, __data_end__, __bss_start__, __bss_end__;
+    uint32_t *src, *dst;
+
+    src = &_data_loadaddr;
+    dst = &__data_start__;
+    while (dst < &__data_end__) {
+        *dst++ = *src++;
+    }
+
+    dst = &__bss_start__;
+    while (dst < &__bss_end__) {
+        *dst++ = 0;
+    }
+
+    /* Call main() */
+    main();
+
+    /* Hang */
+    while (1);
+}
