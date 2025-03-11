56ca3ad4aaf70bb3998226cb810a449167ef3f33
MarcoStucchi <marco.stucchi@gmail.com>
2025-03-11 02:29:47 +0100

    Added variable current support and unified test_case_launch() function

--- tests/test_protection_overload.c
+++ tests/test_protection_overload.c
@@ -5,10 +5,19 @@
 #include "protection_overload.h"
 
 #define TEST_MAX_TIME   3600.0  // [s] max time for test execution (default = 1 hour)
+#define END_SIMULATION  -1.0f   // End of simulated current values
 
+// Simulated current single element
+typedef struct {
+    float time;
+    float current;
+} t_simulated_current_element;
+
+// Test case structure
 typedef struct {
     unsigned int id;
     float current;
+    const t_simulated_current_element *simulated_currents;
     ProtectionOverloadState expected_state;
     float expected_time;
     const char* description;
@@ -19,7 +28,6 @@ int mock_index = 0;
 
 // Test current value (mocked sensor value)
 float test_current = 0.0f;
-
 
 
 /* ------------------------------------------------ 
@@ -43,13 +51,27 @@ float Sensor_Read() {
     return test_current;
 }
 
+float Sensor_Read_Mock(t_simulated_current_element *simulated_currents, float time) {
+    
+    float current = 0.0f;
+    
+    // Find the current value for the given time
+    for (int i = 0; simulated_currents[i].time != END_SIMULATION; i++) {
+        if (time >= simulated_currents[i].time) {
+            current = simulated_currents[i].current;
+        }
+    }
+    return current;
+}
+
 /* ------------------------------------------------ 
         Test Functions
    ------------------------------------------------ */
 
 void test_ProtectionOverload_Generic(
     ProtectionOverloadParams *params,
-    float simulated_current,    
+    float simulated_current,
+    const t_simulated_current_element *simulated_currents,    
     ProtectionOverloadState expected_state,
     float expected_time) {
 
@@ -73,6 +95,15 @@ void test_ProtectionOverload_Generic(
     // ! Exit conditions are only a tripped protection or maximum test time reached
     while (ProtectionOverload_SM_GetState() != ST_OVERLOAD_TRIGGERED && iterations < max_iterations) {
 
+        // Set test current value
+        if (simulated_currents != NULL) {
+            // Get current time
+            float current_time = iterations * ProtectionOverload_SM_GetCallRate();
+        
+            // Get current value from simulated values
+            test_current = Sensor_Read_Mock(simulated_currents, current_time);
+        }
+
         // Run State Machine
         ProtectionOverload_SM_Run();
 
@@ -92,8 +123,9 @@ void test_ProtectionOverload_Generic(
     }
 }
 
+
 /* ------------------------------------------------ 
-        Test Cases - Fixed Current Values
+        Test Parameters
    ------------------------------------------------ */
 
 // Protection parameters
@@ -105,30 +137,48 @@ ProtectionOverloadParams protectionParams = {
     .max_energy = 1.0f              // 1.0 is the trip threshold
 };
 
-const t_test_case test_cases_fixed_current[] = {
-    {.id = 100, .current = 0.2f, .expected_state = ST_IDLE, .description = "Low current"},
-    {.id = 101, .current = 0.8f, .expected_state = ST_IDLE, .description = "Normal current"},
-    {.id = 102, .current = 1.0f, .expected_state = ST_IDLE, .description = "Nominal current"},
-    {.id = 103, .current = 1.2f, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 2.27f, .description = "Overload current 1,2 x Itrip"},
-    {.id = 104, .current = 1.4f, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 1.04f, .description = "Overload current 1,4 x Itrip"},
-    {.id = 105, .current = 1.6f, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 0.64f, .description = "Overload current 1,6 x Itrip"},
-    {.id = 106, .current = 2.0f, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 0.33f, .description = "Overload current 2,0 x Itrip"},
-    {.id = 107, .current = 3.0f, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 0.12f, .description = "Overload current 3,0 x Itrip"}
- };
+/* ------------------------------------------------ 
+        Test Case Launch Function
+   ------------------------------------------------ */
 
-void test_case_launch(unsigned int test_id) {
-    printf("Running test case %d: %s\n", test_cases_fixed_current[test_id].id, test_cases_fixed_current[test_id].description);
-    test_ProtectionOverload_Generic(&protectionParams, test_cases_fixed_current[test_id].current, test_cases_fixed_current[test_id].expected_state, test_cases_fixed_current[test_id].expected_time);
+void test_case_launch(const t_test_case *test_case) {
+    // printf("Running test case %d: %s\n", test_case->id, test_case->description);
+    
+    // lunche the actual test
+    test_ProtectionOverload_Generic(
+        &protectionParams, 
+        test_case->current, 
+        test_case->simulated_currents, 
+        test_case->expected_state, 
+        test_case->expected_time);
 }   
 
-void test_fixed_current_100(void) {test_case_launch(0);}
-void test_fixed_current_101(void) {test_case_launch(1);}
-void test_fixed_current_102(void) {test_case_launch(2);}
-void test_fixed_current_103(void) {test_case_launch(3);}
-void test_fixed_current_104(void) {test_case_launch(4);}
-void test_fixed_current_105(void) {test_case_launch(5);}
-void test_fixed_current_106(void) {test_case_launch(6);}
-void test_fixed_current_107(void) {test_case_launch(7);}
+
+/* ------------------------------------------------ 
+        Test Cases - Fixed Current Values
+   ------------------------------------------------ */
+
+const t_test_case test_cases_fixed_current[] = {
+    {.id = 100, .current = 0.2f, .simulated_currents = NULL, .expected_state = ST_IDLE, .description = "Low current"},
+    {.id = 101, .current = 0.8f, .simulated_currents = NULL, .expected_state = ST_IDLE, .description = "Normal current"},
+    {.id = 102, .current = 1.0f, .simulated_currents = NULL, .expected_state = ST_IDLE, .description = "Nominal current"},
+    {.id = 103, .current = 1.2f, .simulated_currents = NULL, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 2.27f, .description = "Overload current 1,2 x Itrip"},
+    {.id = 104, .current = 1.4f, .simulated_currents = NULL, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 1.04f, .description = "Overload current 1,4 x Itrip"},
+    {.id = 105, .current = 1.6f, .simulated_currents = NULL, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 0.64f, .description = "Overload current 1,6 x Itrip"},
+    {.id = 106, .current = 2.0f, .simulated_currents = NULL, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 0.33f, .description = "Overload current 2,0 x Itrip"},
+    {.id = 107, .current = 3.0f, .simulated_currents = NULL, .expected_state = ST_OVERLOAD_TRIGGERED, .expected_time = 0.12f, .description = "Overload current 3,0 x Itrip"}
+ };
+
+/* ------------------------------------------------ 
+
+void test_fixed_current_100(void) {test_case_launch(&test_cases_fixed_current[0]);}
+void test_fixed_current_101(void) {test_case_launch(&test_cases_fixed_current[1]);}
+void test_fixed_current_102(void) {test_case_launch(&test_cases_fixed_current[2]);}
+void test_fixed_current_103(void) {test_case_launch(&test_cases_fixed_current[3]);}
+void test_fixed_current_104(void) {test_case_launch(&test_cases_fixed_current[4]);}
+void test_fixed_current_105(void) {test_case_launch(&test_cases_fixed_current[5]);}
+void test_fixed_current_106(void) {test_case_launch(&test_cases_fixed_current[6]);}
+void test_fixed_current_107(void) {test_case_launch(&test_cases_fixed_current[7]);}
 
 /* ------------------------------------------------ 
         Main Function
